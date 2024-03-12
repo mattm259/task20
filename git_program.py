832835311d361b7ab93490f1f635b85868c76515
Matt <mattjmeadows259@gmail.com>
2024-03-12 01:19:32 +0000

    Variable renamed

---
+++ git_program.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 12 01:09:54 2024
+
+@author: mattmeadows
+"""
+
+user_input_name = input("Enter your name: ")
+print("Welcome" + user_input_name)
